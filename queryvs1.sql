619e34f2a0b8c27fd0036950784fe7b7ed20346e
Gabriel Christino <134338773+christinogabriel@users.noreply.github.com>
2023-11-07 11:44:44 -0300

    V1.0.0  Base-Banco de dados

---
+++ queryvs1.sql
@@ -0,0 +1,15 @@
+ALTER TABLE comida
+ADD nome VARCHAR(20);
+
+CREATE TABLE COMIDA (
+tempero VARCHAR(50),
+ingredientes VARCHAR(50),
+receita VARCHAR(50)
+);
+
+CREATE TABLE USUARIO (
+nome VARCHAR(50),
+email VARCHAR (50),
+senha INT(10)
+);
+
