5099b59ae35aecf1b08585fe119ce5f729cc3ecd
Mateus Henrique, Douglas, Thalles, Marllon e Jonathan <mateushprofissional@gmail.com>
2023-11-21 09:38:12 -0300

    V0.2.1 - Correção erro user repetido

--- MySQL/comandos2.sql
+++
@@ -1,7 +0,0 @@
-#CREATE DATABASE comidaria;
-USE comidaria;
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
